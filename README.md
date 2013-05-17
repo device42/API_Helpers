2095062e1610f32d1d92db2ef5c1dd736a281598
Raj Jalan <rjalan2@gmail.com>
2013-05-17 05:58:02 -0400

    Easier edit and better error messages.

--- README.md
+++ README.md
@@ -1,9 +1,28 @@
 [Device42](http://www.device42.com/) is a comprehensive data center management software.
 
-This project contains sample scripts that provide helper functions to easily input data to device42 appliance via APIs.
+This repository hosts a python script to easily input data to device42 appliance via APIs.
 
 
-## Scripts Provided
+### Script Provided
 -----------------------------
    * csv2d42apis.py : Reads a CSV file, matches columns to arguments for APIs and sends data to device42 via POST or PUT.
 
+
+### Usage
+-----------------------------
+
+You will need to Change the following 2 sections in the script to match your environment and csv file columns.
+This is assuming you have the csv file ready with data to send to device42.
+
+1. Enter your device42 appliance url, credentials and the url for the API call.
+    * `D42_API_URL` will be the d42 instance base url plus the api call url. API call urls available at: [http://docs.device42.com/api/](http://docs.device42.com/api/)
+    * `D42_USERNAME` and 'D42_PASSWORD' are self explanatory
+    * `API_METHOD` will be put or post, depending on the call. (As found in documentation linked above)
+    * `CSV_FILE_NAME` will be the name of the csv file with data.
+    * `DEBUG` can be changed to True or False, depending on how verbose you want the output to be.
+
+2. Match the CSV columns to corresponding API argument. CSV indexes are zero based.
+    * e.g. `args = {'name': row_values[0], 'key': row_values[1]}` assumes first column is name and second column is key.
+    * use `if row_values[x]` if the given field might be empty for certain rows.
+
+Once the file is ready, you can execute it. In the end, it will print all the rows that are added in `added` and all the rows that failed  in `notadded`. `notadded` will have the reason for the fail as well.
--- csv2d42apis.py
+++ csv2d42apis.py
@@ -9,7 +9,7 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 '''
 
 ##############################################
-# csv file to api helper utility
+# csv file to api helper utility v2.0
 # create csv, match the column values with arguments in changerow_to_api_args function call
 # and upload data to device42 via REST APIs
 ##############################################
@@ -20,33 +20,33 @@ import base64
 import csv
 
 ##### Change Following lines to match your environment #####
-d42url = 'https://your-d42-url-here'
-urluser = 'your-d42-username-here'
-urlpass = 'your-d42-password-here'
+### API URLS available at http://docs.device42.com/apis/ ###
 
-def post(url, method, params):
-    result = ''
+D42_API_URL = 'https://your-d42-fqdn-or-ip/api/1.0/custom_fields/appcomp/'  #make sure to end in /
+D42_USERNAME = 'your-d42-username-here'
+D42_PASSWORD = 'your-d42-password-here'
+API_METHOD = 'put'                                                          #whether you are doing a put or post call.
+CSV_FILE_NAME = 'file_name.csv'                                             #name of the file with the values being uploaded
+DEBUG = True                                                                #True or False. True for detailed info per call
+
+def post(params):
     data= urllib.urlencode(params)
     headers = {
-            'Authorization' : 'Basic '+ base64.b64encode(urluser + ':' + urlpass),
+            'Authorization' : 'Basic '+ base64.b64encode(D42_USERNAME + ':' + D42_PASSWORD),
             'Content-Type' : 'application/x-www-form-urlencoded'
         }
-
-    req = urllib2.Request(url, data, headers)
-
-    print '---REQUEST---',req.get_full_url()
-    print req.headers
-    print req.data
-
-    if method == 'put': req.get_method = lambda: 'PUT'
-
-    response = urllib2.urlopen(req)
-
-    print '---RESPONSE---'
-    print response
-    result = str(response.read())
-
-    return result
+    req = urllib2.Request(D42_API_URL, data, headers)
+    if DEBUG: print '---REQUEST---',req.get_full_url()
+    if DEBUG: print req.headers
+    if DEBUG: print req.data
+    if API_METHOD == 'put': req.get_method = lambda: 'PUT'
+    try:
+        urllib2.urlopen(req)
+        return True, ''
+    except urllib2.HTTPError, e:
+        error_response = e.read()
+        if DEBUG: print e.code, error_response
+        return False, error_response
 
 def to_ascii(s): #not used in example, but provided incase you would need to convert certain values to ascii
     """remove non-ascii characters"""
@@ -55,46 +55,39 @@ def to_ascii(s): #not used in example, but provided incase you would need to con
     else:
         return str(s)
 
-
 def changerow_to_api_args(row_values):
+
     #map each row value to corresponding API argument.
     #row index starts from zero.
     #change this to match your CSV column values to API arguments to map to.
     #following example is for application component custom key pair values.
     #csv file columns are: application component name, key, value, value2, show on chart, notes
     #mapping index would be:  0                     , 1    , 2,     3,      4,              5
-    #in this example, since row_values[0] and row_values[1] are required, we would raise exception if these are empty.
-    if not row_values[0] or not row_values[1]: raise Exception('Required Parameter Missing')
     #create a dictionary from values, that can be passed to api call.
+
     args = {'name': row_values[0], 'key': row_values[1]}
-    if row_values[2]: args.update({'value': row_values[2]})
+    if row_values[2]: args.update({'value': row_values[2]})         #use if condition if the value can be empty
     if row_values[3]: args.update({'value2': row_values[3]})
     if row_values[4]: args.update({'show_on_chart': row_values[4]})
     if row_values[5]: args.update({'notes': row_values[5]})
     return args
 
 def read_csv_parse_and_call_api_function(filename):
-    #API url. Checks if there is ending / in the original url.
-    if d42url[:-1] == '/':
-        API_URL = d42url + 'api/1.0/custom_fields/appcomp/'
-    else:
-        API_URL = d42url + '/api/1.0/custom_fields/appcomp/'
-    API_METHOD = 'put' #whether you are doing a put or post call.
     notadded = []
     added = []
     with open(filename, 'rb') as csvfile:
         ReadLine = csv.reader(csvfile)
-
         ReadLine.next() #skip the header row.
         for i in ReadLine:
-            if i != []:
+            if i:
                 try:
                     args = changerow_to_api_args(i)
-                    post(API_URL, API_METHOD, args)
-                    added.append(i)
+                    ADDED, msg = post(args)
+                    if ADDED: added.append(i)
+                    else: notadded.append(i+[' ',msg])
                 except Exception, Err:
                     notadded.append(i+[str(Err),])
     print 'notadded %s' % notadded
     print 'added %s' % added
 
-read_csv_parse_and_call_api_function('file_name.csv') #the actual call with the csv file name.
+read_csv_parse_and_call_api_function(CSV_FILE_NAME) #the actual call with the csv file name.
